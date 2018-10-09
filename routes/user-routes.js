518d7df33ccbd3e46bb84505c31714d9df642aca
Jonathan Harris <yochananh@gmail.com>
2018-10-09 16:26:42 -0400

    deploying

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const User = require("../models/User");
 const uploadCloud = require("../config/cloudinary.js");
 
+console.log("something random for this to work!!!!!");
+
 module.exports = router;
