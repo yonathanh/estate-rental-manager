e7ffea9ecb45c5b72e63f88c68df28fb1fa74410
Jonathan Harris <yochananh@gmail.com>
2018-10-09 15:46:02 -0400

    deployment

--- client/src/components/Properties/PropertyPage/PropertyPage.jsx
+++ client/src/components/Properties/PropertyPage/PropertyPage.jsx
@@ -15,7 +15,7 @@ class PropertyPage extends Component {
   getSingleProperty = () => {
     const { params } = this.props.match;
     axios
-      .get(`${process.env.BASE_URL}properties/${params.id}`)
+      .get(`${process.env.BASE_URL}/properties/${params.id}`)
       .then(responseFromApi => {
         // console.log("responseFromApi.data", responseFromApi.data);
         const theProperty = responseFromApi.data;
